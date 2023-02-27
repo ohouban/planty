ca62e5fcfc1054efd67cdbd2563e54dd89525350
ohouban <houban.oumaima@gmail.com>
2023-02-27 20:49:31 +0100

    add assets and modify functions

--- functions.php
+++ functions.php
@@ -13,8 +13,8 @@ function theme_enqueue_styles()
 
 function mytheme_register_assets()
 {
-    wp_register_style('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css', []);
-    wp_register_script('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js', ['popper', 'jquery'], false, true);
+    wp_register_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css', []);
+    wp_register_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js', ['popper', 'jquery'], false, true);
     wp_register_script('popper', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js', [], false, true);
     wp_deregister_script('jquery');
     wp_register_script('jquery', 'https://code.jquery.com/jquery-3.2.1.slim.min.js', [], false, true);
--- header.php
+++ header.php
@@ -5,40 +5,33 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">-->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
   <title>Planty</title>
   <?php wp_head() ?>
 </head>
-
-<body>
-  <!DOCTYPE html>
-  <html lang="fr-FR">
-
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <title>Planty</title>
-    <?php wp_head() ?>
-  </head>
-
   <body>
     <header>
-      <nav class="navbar navbar-expand-lg p-0">
-        <div class="container-fluid pr-0">
+      <nav class="navbar navbar-expand-lg navbar-expand-sm p-0">
+      <div class="container-fluid pe-0">
           <a class="navbar-brand" href="http://planty.local/accueil">
             <img src="http://planty.local/wp-content/uploads/2022/12/Logo.png" width="180" alt="logo planty">
           </a>
-          <div class="collapse navbar-collapse menu" id="navbarSupportedContent">
-            <?php wp_nav_menu([
-            'theme_location' => 'header',
-            'container' => false,
-            'menu_class' => 'navbar-nav ml-auto'
-          ]) ?>
-            <li>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        
+          <div class="collapse navbar-collapse justify-content-end menu" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto mb-lg-0">
+              <?php wp_nav_menu([
+                'theme_location' => 'header',
+                'container' => false,
+                'menu_class' => 'navbar-nav'
+              ]) ?>
+        
               <a class="btnNav" style="color:white;" href="http://planty.local/precommande">Commander</a>
-            </li>
+            
+            </ul> 
           </div>
         </div>
       </nav>
